55b1c8bc9c034e3c3eb81f86af6c75d515161951
guojun <jun4.guo@zhangmen.com>
2023-08-31 18:45:01 +0800

    feat: init

---
+++ components/custom-icon/custom-icon.vue
@@ -0,0 +1,8 @@
+<template>
+  <uni-icons type="contact" size="30" />
+  <uni-icons custom-prefix="iconfont" type="icon-uniyonghu" size="30" />
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
